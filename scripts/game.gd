aeeff4821171c322d86bcdd3cd442c8c5e2f58b9
Eylon Keshet <>
2024-09-23 19:40:34 +0300

    note scene, note hits player

--- scripts/game.gd
+++ scripts/game.gd
@@ -12,12 +12,16 @@ class_name Game extends Node2D
 
 static var game_scene: String = "res://scenes/game.tscn"
 static var game_over_scene: String = "res://scenes/game_over_screen.tscn"
+static var game_won_scene: String = "res://scenes/game_won_screen.tscn"
 
-var PlayerHealth: float = 10
+var player_health: float = 10
 var BossHealth: float = 10
 var DamageFromBoss: float = 1
 var DamageFromPlayer: float = 1
 var starting_position: Vector2
+var vul_time: float = 2
+var time_elapsed: float = 0
+var vulnerable: bool = false
 
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -25,29 +29,49 @@ func _unhandled_input(event: InputEvent) -> void:
 		music_player.pitch_scale += 0.2
 	elif event.is_action_pressed("ui_down"):
 		music_player.pitch_scale -= 0.2
+	if event.is_action_pressed("ui_accept"):
+		win()
 
 
 func _ready() -> void:
 	starting_position = ending_point.position
 	music_player.play(100)
-	pass # Replace with function body.
+	player_health_bar.max_value = player_health
+	player_health_bar.value = player_health
+	boss_health_bar.max_value = BossHealth
+	boss_health_bar.value = BossHealth
 
 
 func _process(delta: float) -> void:
-	#if not music_player.playing:
-		#music_player.play(110)
+	time_elapsed += delta
+	if time_elapsed > vul_time:
+		vulnerable = true
 	var stream: AudioStream = music_player.stream
 	var song_length: float = stream.get_length()
 	var normalized_song_position: float = music_player.get_playback_position() / song_length
-	#print(normalized_song_position)
-	
 	ending_point.position.x = lerp(starting_position.x+6000,-200.0,normalized_song_position)
 
 
 func _on_music_player_finished() -> void:
 	print("finished!")
-	lose()
+	#lose()
 
 func lose() -> void:
 	print("you lost")
 	get_tree().change_scene_to_file("res://scenes/game_over_screen.tscn")
+
+func win() -> void:
+	print("you lost")
+	get_tree().change_scene_to_file("res://scenes/game_won_screen.tscn")
+
+
+func _on_hit_zone_body_entered(body: CollisionObject2D) -> void:
+	if vulnerable:
+		print("OUCH!")
+		player_health -= 5
+		player_health_bar.value = player_health
+	else:
+		print("false hit")
+
+func on_level_ready() -> void:
+	print("level ready detected!")
--- scripts/notes_container.gd
+++ scripts/notes_container.gd
@@ -3,11 +3,17 @@ extends Sprite2D
 @onready var ending_point: Node2D = $".."
 var starting_position_x: float
 var size: Vector2 = texture.get_size()
+var game: Game
+
+signal level_ready
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	game = get_tree().get_root().get_node("Game")
+	level_ready.connect(game.on_level_ready)
 	starting_position_x = size.x / 2
 	set_parent_at_ending()
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,3 +22,4 @@ func _process(delta: float) -> void:
 
 func set_parent_at_ending() -> void:
 	position.x -= size.x / 2
+	emit_signal("level_ready")
