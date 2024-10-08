4afd28b0353776bb67f1534436121361aaa4b894
Eylon Keshet <>
2024-10-08 15:22:13 +0300

    basic start screen

--- scripts/start_screen.gd
+++ scripts/start_screen.gd
@@ -1,9 +1,18 @@
 extends Node2D
 
 
+@onready var load_overlay: TextureRect = $UI/LoadOverlay
 
+
+func _ready() -> void:
+	load_overlay.visible = false
 
 func _on_button_button_up() -> void:
+	load_overlay.visible = true
+	var timer: Timer = Timer.new()
+	add_child(timer)
+	timer.start()
+	await timer.timeout
 	var new_scene_source: PackedScene = load(Game.game_scene)
 	Game.game_state = "Playing"
 	get_tree().change_scene_to_packed(new_scene_source)
