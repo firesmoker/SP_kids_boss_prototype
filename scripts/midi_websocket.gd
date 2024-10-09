ce373b0bd5c6d3d3216c550de4039f29c0e78c09
Alon Raz Levi <levi.alon.il@gmail.com>
2024-10-09 10:29:57 +0300

    wip

--- scripts/midi_websocket.gd
+++ scripts/midi_websocket.gd
@@ -10,7 +10,7 @@ enum WebSocketMessageList {
 }
 
 func _ready() -> void:
-	socket.connect_to_url("ws://Eylons-Mackbook-Pro.local:8099")
+	socket.connect_to_url("ws://h-MacBook-Pro-sl-Simply.local:8099")
 
 func _process(delta: float) -> void:
 	socket.poll()
