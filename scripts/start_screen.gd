bb2fff0799e0615fbfc99a9868eac4dff927ae81
Eylon Keshet <>
2024-10-14 14:51:25 +0300

    more levels

--- scripts/start_screen.gd
+++ scripts/start_screen.gd
@@ -3,18 +3,34 @@ extends Node2D
 
 @onready var load_overlay: TextureRect = $UI/LoadOverlay
 var new_scene_source: PackedScene = preload("res://scenes/game.tscn")
-@export_category("Easy")
-@export var easy_song_path: String
-@export var easy_slow_song_path: String
-@export var easy_melody_path: String
-@export_category("Normal")
-@export var normal_song_path: String
-@export var normal_slow_song_path: String
-@export var normal_melody_path: String
-@export_category("Hard")
-@export var hard_song_path: String
-@export var hard_slow_song_path: String
-@export var hard_melody_path: String
+@export_category("Just Can't Wait")
+@export var song_1_path: String
+@export var song_1_slow_song_path: String
+@export var song_1_melody_path: String
+@export_category("Circle of Life")
+@export var song_2_path: String
+@export var song_2_slow_song_path: String
+@export var song_2_melody_path: String
+@export_category("Let it Go")
+@export var song_3_song_path: String
+@export var song_3_slow_song_path: String
+@export var song_3_melody_path: String
+@export_category("Hokey Pokey")
+@export var song_4_song_path: String
+@export var song_4_slow_song_path: String
+@export var song_4_melody_path: String
+@export_category("Free Fallin")
+@export var song_5_song_path: String
+@export var song_5_slow_song_path: String
+@export var song_5_melody_path: String
+@export_category("Dance Monkey")
+@export var song_6_song_path: String
+@export var song_6_slow_song_path: String
+@export var song_6_melody_path: String
+@export_category("Pokemon")
+@export var song_7_song_path: String
+@export var song_7_slow_song_path: String
+@export var song_7_melody_path: String
 
 func _ready() -> void:
 	load_overlay.visible = false
@@ -41,13 +57,32 @@ func start_level(melody_path: String, song_path: String,
 	get_tree().change_scene_to_packed(new_scene_source)
 
 
-func _on_easy_button_button_up() -> void:
-	start_level(easy_melody_path,easy_song_path,easy_slow_song_path, 76, 8, 29)
-
-
-func _on_normal_button_button_up() -> void:
-	start_level(normal_melody_path,normal_song_path,normal_slow_song_path,78, 4, 15)
 	
 
-func _on_hard_button_button_up() -> void:
-	start_level(hard_melody_path,hard_song_path,hard_slow_song_path,106,5, 20)
+
+func _on_button1_button_up() -> void:
+	start_level(song_1_melody_path,song_1_path,song_1_slow_song_path, 76, 8, 29)
+
+
+func _on_button2_button_up() -> void:
+	start_level(song_2_melody_path,song_2_path,song_2_slow_song_path,78, 4, 15)
+
+
+func _on_button3_button_up() -> void:
+	start_level(song_3_melody_path,song_3_song_path,song_3_slow_song_path,106,5, 20)
+
+
+func _on_button_4_button_up() -> void:
+	start_level(song_4_melody_path,song_4_song_path,song_4_slow_song_path,88,5, 20)
+
+
+func _on_button_5_button_up() -> void:
+	start_level(song_5_melody_path,song_5_song_path,song_5_slow_song_path,155,5, 20)
+
+
+func _on_button_6_button_up() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_7_button_up() -> void:
+	pass # Replace with function body.
