182096fa1fda6a702565b1b2681d6d82a2301ed4
Alon Raz Levi <levi.alon.il@gmail.com>
2024-10-15 15:22:52 +0300

    Fixed midi bug

--- scripts/midi_processor.gd
+++ scripts/midi_processor.gd
@@ -9,7 +9,7 @@ class_name MidiProcessor
 # List to store all note detectors
 @onready var note_detectors: Array = [notes_detector, bottom_notes_detector, collect_detect, bottom_collect_detect]
 
-const OCTAVE_KEY_INDEX: Array = ["C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", "G4", "G#4", "A4", "A#4", "B4"]
+const OCTAVE_KEY_INDEX: Array = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -24,12 +24,14 @@ func processEvent(event: MidiEvent) -> void:
 	
 	var key_index: int = event.pitch % 12
 	var note: String = OCTAVE_KEY_INDEX[key_index]
+	var octave: int = event.pitch / 12 - 1
+	var full_note: String = str(note) + str(octave)
 	match event.action:
 		"on":
 			print("MIDI: " + event.as_string())
 			# Call note_played on all detectors in the list
 			for detector: Node in note_detectors:
-				detector.note_played(note)
+				detector.note_played(full_note)
 
 		"off":
 			pass
