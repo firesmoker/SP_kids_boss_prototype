5bef597b272184f0fd697042e6f720dd5f7b65e9
Eylon Keshet <>
2024-11-04 14:44:16 +0200

    user facing difficulty screen

--- scripts/start_screen.gd
+++ scripts/start_screen.gd
@@ -6,17 +6,20 @@ extends Node2D
 #@onready var normal_button: Button = $UI/Difficulty/NormalButton
 #@onready var hard_button: Button = $UI/Difficulty/HardButton
 @onready var song_buttons: ScrollContainer = $UI/SongButtons
-@onready var auto_play_toggle: CheckButton = $UI/AutoPlayToggle
-@onready var library_song_toggle: CheckButton = $UI/LibrarySongToggle
-@onready var debug_toggle: CheckButton = $UI/DebugToggle
-@onready var skip_intro: CheckButton = $"UI/Skip Intro"
-@onready var skip_middle_c: CheckButton = $UI/SkipMiddleC
+@onready var auto_play_toggle: CheckButton = $UI/DevButtons/AutoPlayToggle
+@onready var library_song_toggle: CheckButton = $UI/DevButtons/LibrarySongToggle
+@onready var debug_toggle: CheckButton = $UI/DevButtons/DebugToggle
+@onready var skip_intro: CheckButton = $"UI/DevButtons/Skip Intro"
+@onready var skip_middle_c: CheckButton = $UI/DevButtons/SkipMiddleC
+@onready var dev_buttons: Control = $UI/DevButtons
+@onready var song_title: Label = $UI/Difficulty/SongTitle
 
 
 
 @onready var load_overlay: TextureRect = $UI/LoadOverlay
 var default_left_melody: String = "res://levels/melody1_left.txt"
 @export_group("Just Can't Wait")
+@export var song_1_title: String = "Just Can't Wait"
 @export var song_1_path: String
 @export var song_1_slow_song_path: String
 @export var song_1_melody_path: String
@@ -25,44 +28,52 @@ var default_left_melody: String = "res://levels/melody1_left.txt"
 @export var song_1_easy_melody_path: String
 @export var song_1_hard_melody_path: String
 @export_group("Circle of Life")
+@export var song_2_title: String = "Circle of Life"
 @export var song_2_path: String
 @export var song_2_slow_song_path: String
 @export var song_2_melody_path: String
 @export var song_2_easy_melody_path: String
 @export var song_2_hard_melody_path: String
 @export_group("Let it Go")
+@export var song_3_title: String = "Let it Go"
 @export var song_3_song_path: String
 @export var song_3_slow_song_path: String
 @export var song_3_melody_path: String
 @export var song_3_hard_melody_path: String
 @export_group("Hokey Pokey")
+@export var song_4_title: String = "Hokey Pokey"
 @export var song_4_song_path: String
 @export var song_4_slow_song_path: String
 @export var song_4_melody_path: String
 @export var song_4_hard_melody_path: String
 @export_group("Free Fallin")
+@export var song_5_title: String = "Free Fallin"
 @export var song_5_song_path: String
 @export var song_5_slow_song_path: String
 @export var song_5_melody_path: String
 @export var song_5_hard_melody_path: String
 @export_group("Dance Monkey")
+@export var song_6_title: String = "Dance Monkey"
 @export var song_6_song_path: String
 @export var song_6_slow_song_path: String
 @export var song_6_melody_path: String
 @export var song_6_left_melody_path: String
 @export var song_6_left_hard_melody_path: String
 @export_group("Pokemon")
+@export var song_7_title: String = "Pokemon"
 @export var song_7_song_path: String
 @export var song_7_slow_song_path: String
 @export var song_7_melody_path: String
 @export var song_7_left_melody_path: String
 @export var song_7_left_hard_melody_path: String
 @export_group("Believer")
+@export var song_8_title: String = "Believer"
 @export var song_8_song_path: String
 @export var song_8_slow_song_path: String
 @export var song_8_melody_path: String
 @export var song_8_left_melody_path: String
 @export_group("Jingle Bells")
+@export var song_9_title: String = "Jingle Bells"
 @export var song_9_song_path: String
 @export var song_9_slow_song_path: String
 @export var song_9_melody_path: String
@@ -70,11 +81,13 @@ var default_left_melody: String = "res://levels/melody1_left.txt"
 @export var song_9_hard_melody_path: String
 @export var song_9_left_hard_melody_path: String
 @export_group("Let It March!")
+@export var song_10_title: String = "Let It March!"
 @export var song_10_song_path: String
 @export var song_10_slow_song_path: String
 @export var song_10_melody_path: String
 @export var song_10_hard_melody_path: String
 @export_group("I Just Can't Wait to be TECHNO")
+@export var song_11_title: String = "I Just Can't Wait to be TECHNO"
 @export var song_11_song_path: String
 @export var song_11_slow_song_path: String
 @export var song_11_melody_path: String
@@ -86,6 +99,7 @@ var maximum_input_distance: float = 100
 var current_press_position: Vector2
 
 func _ready() -> void:
+	dev_buttons.visible = true
 	load_overlay.visible = false
 	darken.visible = false
 	difficulty.visible = false
@@ -129,6 +143,7 @@ func _on_button1_button_up() -> void:
 	if unpressed_accepted():
 		show_difficulty_buttons(true)
 		Game.has_easy_difficulty = true
+		song_title.text = song_1_title
 		define_easy_level(song_1_easy_melody_path,default_left_melody,song_1_easy_path,song_1_easy_slow_song_path, 70, 6, 24,3, "treble")
 		define_level(song_1_melody_path,default_left_melody,song_1_path,song_1_slow_song_path, 76, 8, 30,2.5, "treble")
 		define_hard_level(song_1_hard_melody_path,default_left_melody,song_1_path,song_1_slow_song_path, 76, 6, 31, 2.5, "treble")
@@ -137,6 +152,7 @@ func _on_button1_button_up() -> void:
 func _on_button2_button_up() -> void:
 	if unpressed_accepted():
 		show_difficulty_buttons(true)
+		song_title.text = song_2_title
 		Game.has_easy_difficulty = true
 		define_level(song_2_melody_path,default_left_melody,song_2_path,song_2_slow_song_path,78, 7, 26,3.3, "treble")
 		define_easy_level(song_2_easy_melody_path,default_left_melody,song_2_path,song_2_slow_song_path,78, 6, 22,3, "treble")
@@ -146,6 +162,7 @@ func _on_button2_button_up() -> void:
 func _on_button3_button_up() -> void:
 	if unpressed_accepted():
 		show_difficulty_buttons()
+		song_title.text = song_3_title
 		Game.has_easy_difficulty = false
 		define_level(song_3_melody_path,default_left_melody,song_3_song_path,song_3_slow_song_path,106,5, 20, 3)
 		define_easy_level(song_3_melody_path,default_left_melody,song_3_song_path,song_3_slow_song_path,106,5, 20, 3)
@@ -155,6 +172,7 @@ func _on_button3_button_up() -> void:
 func _on_button_4_button_up() -> void:
 	if unpressed_accepted():
 		show_difficulty_buttons()
+		song_title.text = song_4_title
 		Game.has_easy_difficulty = false
 		define_level(song_4_melody_path,default_left_melody,song_4_song_path,song_4_slow_song_path,88,9,35)
 		define_easy_level(song_4_melody_path,default_left_melody,song_4_song_path,song_4_slow_song_path,88,9,35)
@@ -164,6 +182,7 @@ func _on_button_4_button_up() -> void:
 func _on_button_5_button_up() -> void:
 	if unpressed_accepted():
 		show_difficulty_buttons()
+		song_title.text = song_5_title
 		Game.has_easy_difficulty = false
 		define_level(song_5_melody_path,default_left_melody,song_5_song_path,song_5_slow_song_path,155,13, 51)
 		define_easy_level(song_5_melody_path,default_left_melody,song_5_song_path,song_5_slow_song_path,155,13, 51)
@@ -173,6 +192,7 @@ func _on_button_5_button_up() -> void:
 func _on_button_6_button_up() -> void:
 	if unpressed_accepted():
 		show_difficulty_buttons()
+		song_title.text = song_6_title
 		Game.has_easy_difficulty = false
 		define_level(song_6_melody_path,song_6_left_melody_path,song_6_song_path,song_6_slow_song_path,85,6, 23,2.5, "both")
 		define_easy_level(song_6_melody_path,song_6_left_melody_path,song_6_song_path,song_6_slow_song_path,85,6, 23,2.5, "both")
@@ -182,6 +202,7 @@ func _on_button_6_button_up() -> void:
 func _on_button_7_button_up() -> void:
 	if unpressed_accepted():
 		show_difficulty_buttons()
+		song_title.text = song_7_title
 		Game.has_easy_difficulty = false
 		define_level(song_7_melody_path,song_7_left_melody_path,song_7_song_path,song_7_slow_song_path,115,4, 23,2.5, "both")
 		define_easy_level(song_7_melody_path,song_7_left_melody_path,song_7_song_path,song_7_slow_song_path,115,4, 23,2.5, "both")
@@ -191,6 +212,7 @@ func _on_button_7_button_up() -> void:
 func _on_button_8_button_up() -> void:
 	if unpressed_accepted():
 		show_difficulty_buttons()
+		song_title.text = song_8_title
 		Game.has_easy_difficulty = false
 		define_level(song_8_melody_path,song_8_left_melody_path,song_8_song_path,song_8_slow_song_path,110,4, 23,2.5, "both")
 		define_easy_level(song_8_melody_path,song_8_left_melody_path,song_8_song_path,song_8_slow_song_path,110,4, 23,2.5, "both")
@@ -200,6 +222,7 @@ func _on_button_9_button_up() -> void:
 	if unpressed_accepted():
 		print("close!")
 		show_difficulty_buttons()
+		song_title.text = song_9_title
 		Game.has_easy_difficulty = false
 		define_level(song_9_melody_path,song_9_left_melody_path,song_9_song_path,song_9_slow_song_path,88,10, 38,2.5, "both")
 		define_easy_level(song_9_melody_path,song_9_left_melody_path,song_9_song_path,song_9_slow_song_path,88,10, 38,2.5, "both")
@@ -208,6 +231,7 @@ func _on_button_9_button_up() -> void:
 func _on_button10_button_up() -> void:
 	if unpressed_accepted():
 		show_difficulty_buttons()
+		song_title.text = song_10_title
 		Game.has_easy_difficulty = false
 		define_level(song_10_melody_path,default_left_melody,song_10_song_path,song_10_slow_song_path,106,5, 20, 3)
 		define_easy_level(song_10_melody_path,default_left_melody,song_10_song_path,song_10_slow_song_path,106,5, 20, 3)
@@ -217,6 +241,7 @@ func _on_button10_button_up() -> void:
 func _on_button_11_button_up() -> void:
 	if unpressed_accepted():
 		show_difficulty_buttons(true)
+		song_title.text = song_11_title
 		Game.has_easy_difficulty = true
 		define_easy_level(song_11_melody_path,default_left_melody,song_11_song_path,song_11_slow_song_path, 76, 8, 30,2.5, "treble")
 		define_level(song_11_melody_path,default_left_melody,song_11_song_path,song_11_slow_song_path, 76, 8, 30,2.5, "treble")
@@ -281,6 +306,7 @@ func define_hard_level(melody_path: String, left_melody_path: String, song_path:
 	LevelSelector.hard_display_duration = display_duration
 
 func show_difficulty_buttons(show_easy: bool = false) -> void:
+	dev_buttons.visible = false
 	if show_easy:
 		easy_button.visible = true
 	else:
@@ -290,6 +316,7 @@ func show_difficulty_buttons(show_easy: bool = false) -> void:
 	difficulty.visible = true
 
 func hide_difficulty_buttons() -> void:
+	dev_buttons.visible = true
 	song_buttons.visible = true
 	darken.visible = false
 	difficulty.visible = false
